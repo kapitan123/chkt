8ef774fddffef664c5b355b2d55b67201925948e
andrey kliemnko <andrey.klimenko@verivox.com>
2022-03-22 10:51:55 +0100

    Added healthchecks

---
+++ src/Common/Healthchecks/Healthchecks/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Dapr.Client;
+global using Common.Healthchecks;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using Microsoft.Extensions.Diagnostics.HealthChecks;
